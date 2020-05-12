18d956e26f65b8aed82c67d0df61d85eda0170ae
nadineb <nadineb@umich.edu>
2020-05-11 17:06:08 -0700

    [add] totalDuration

--- models/workout.js
+++ models/workout.js
@@ -4,7 +4,7 @@ const Schema = mongoose.Schema;
 const WorkoutSchema = new Schema({
     day: {
         type: Date,
-        default: Date.now(),
+        default: Date.now,
         required: "Day is Required"
     },
 
@@ -33,8 +33,16 @@ const WorkoutSchema = new Schema({
             type: Number
         }
 
-    }]
+    }],
 
+}, { toJSON: { virtuals: true } });
+
+WorkoutSchema.virtual("totalDuration").get(function() {
+    let total = 0;
+    this.exercises.forEach(exercise => {
+        total += exercise.duration
+    });
+    return total;
 });
 
 const Workout = mongoose.model("Workout", WorkoutSchema);
